1551d33716f24e702e30d76271544d47bdfa058e
Sriram Jayashankar <sriramjayashankar@gmail.com>
2023-12-28 17:49:31 +0530

    Add PDF metadata and convert date format

---
+++ filemanager.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from datetime import datetime
+from PyPDF2 import PdfReader
+
+def organize_pdfs(pdf_folder):
+    # Get a list of all PDF files in the folder
+    pdf_files = [f for f in os.listdir(pdf_folder) if f.endswith('.pdf')]
+
+    # Create a directory for each year
+    for pdf_file in pdf_files:
+        file_path = os.path.join(pdf_folder, pdf_file)
+
+        # Extract DecisionDate from the PDF metadata
+        with open(file_path, 'rb') as file:
+            pdf_reader = PdfReader(file)
+            decision_date = pdf_reader.metadata.get('/decisiondate', None)
+
+            if decision_date:
+                try:
+                    decision_date = datetime.strptime(decision_date, '%Y-%m-%d')
+                    year_folder = os.path.join(pdf_folder, str(decision_date.year))
+                    
+                    # Create the year folder if it doesn't exist
+                    if not os.path.exists(year_folder):
+                        os.makedirs(year_folder)
+
+                    # Move the PDF to the corresponding year folder
+                    shutil.move(file_path, os.path.join(year_folder, pdf_file))
+                    print(f'Moved {pdf_file} to {decision_date.year}')
+                except Exception as e:
+                    print(f'Error processing {pdf_file}: {e}')
+            else:
+                print(f'Skipping {pdf_file} as DecisionDate metadata is missing')
+
+# Replace 'path/to/pdfs' with the actual path to your PDFs folder
+pdf_folder_path = 'C:/Users/Sriram/Desktop/programming/Webscraping/Webscrape/testpdfs'
+organize_pdfs(pdf_folder_path)
+
+
+
+
--- test.py
+++ test.py
@@ -1,39 +1,41 @@
-# extract_data.py
+from PyPDF2 import PdfReader,PdfWriter
 
-from bs4 import BeautifulSoup
-import csv
+def get_pdf_metadata(file_path):
+    with open(file_path, 'rb') as file:
+        reader = PdfReader(file)
+        metadata = reader.metadata
+    return metadata
 
-def extract_and_write_cnr_numbers(html_path, file_path):
-    # Read the HTML file
-    with open(html_path, 'r', encoding='utf-8') as file:
-        html_doc = file.read()
+def add_pdf_metadata(file_path, metadata):
+    reader = PdfReader(file_path)
+    writer = PdfWriter()
 
-    soup = BeautifulSoup(html_doc, 'html.parser')
+    for page in reader.pages:
+        writer.add_page(page)
 
-    # Find all elements with class 'caseDetailsTD'
-    case_details_elements = soup.find_all(class_='caseDetailsTD')
+    writer.add_metadata(metadata)
 
-    # Extract data for each entry
-    entries_data = []
-    for element in case_details_elements:
-        print(element)
-        # Uncomment and modify these lines for data extraction
-        # cnr = element.find('span', style='color:#212F3D').find_next('font', color='green').text.strip()
-        # date_of_registration = element.find('span', string='Date of registration :').find_next('font', color='green').text.strip()
-        # decision_date = element.find('span', string='Decision Date :').find_next('font', color='green').text.strip()
-        # disposal_nature = element.find('span', string='Disposal Nature :').find_next('font', color='green').text.strip()
-        # court_name = element.find('span', style='opacity: 0.5;').text.strip()
+    with open(file_path, 'wb') as file:
+        writer.write(file)
 
-        # entries_data.append([cnr, date_of_registration, decision_date, disposal_nature, court_name])
+# Use the function
+#29-06-2047
+#convert the above date into %d-%m-%Y and then add it to the metadata "decisiondate"
+#write a fucntion to take input as date and convert it into %d-%m-%Y
+def convert_date(date):
+    date=date.split('-')
+    date=date[::-1]
+    date='-'.join(date)
+    return date
+#can you give an example of how to use the function
+date='29-06-2047'
+date=convert_date(date)
+print(date)
+metadata = {"/decisiondate":date}
+# Use the function
+pdf_metadata = get_pdf_metadata('C:/Users/Sriram/Desktop/programming/Webscraping/Webscrape/testpdfs/WPA_28047_2023 of DIPTENDU CHATTERJEE Vs UNION OF INDIA AND ORS..pdf')
+metadata.update(pdf_metadata)
 
-    # Write the extracted data to the CSV file
-    with open(file_path, 'a', newline='', encoding='utf-8') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerows(entries_data)
-
-# Specify the path to your local HTML file and the desired output CSV file
-html_path = 'C:/Users/Sriram/Desktop/programming/Webscraping/output_page1.html'
-csv_output_path = 'C:/Users/Sriram/Desktop/programming/Webscraping/cnr_numbers.csv'
-
-# Call the function with the paths
-extract_and_write_cnr_numbers(html_path, csv_output_path)
+add_pdf_metadata('C:/Users/Sriram/Desktop/programming/Webscraping/Webscrape/testpdfs/WPA_28047_2023 of DIPTENDU CHATTERJEE Vs UNION OF INDIA AND ORS..pdf', metadata)
+pdf_metadata_final = get_pdf_metadata('C:/Users/Sriram/Desktop/programming/Webscraping/Webscrape/testpdfs/WPA_28047_2023 of DIPTENDU CHATTERJEE Vs UNION OF INDIA AND ORS..pdf')
+print(pdf_metadata_final)
